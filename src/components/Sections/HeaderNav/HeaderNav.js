6fce92137c3309f5559de82b8fc96eb1700369fc
83mbyte <registered.mail.mb@gmail.com>
2022-11-04 18:25:44 +0200

    modified navigation links

--- src/components/Sections/HeaderNav/HeaderNav.js
+++ src/components/Sections/HeaderNav/HeaderNav.js
@@ -3,44 +3,61 @@ import Logo from '../../Logo/Logo';
 import styles from './HeaderNav.module.css';
 
 const HeaderNav = () => {
+    const navyLinksArray = ['Features', 'Pricing', 'Resources'];
+    const navyButtonsArray = [{ title: 'Register' }, { title: 'SignUp', class: ['aButtonRegister', 'aButtonRegisterDropMenu'] }]
+
     return (
         <Fragment>
             <div className={styles.container}>
 
                 <Logo title={'Shortly'} />
 
-
-
                 <div className={styles.navigation}>
                     <div className={styles.navyLinksContainer}>
-                        <a href='/'>Features</a>
-                        <a href='/'>Pricing</a>
-                        <a href='/'>Resources</a>
-                    </div>
-                    <div className={styles.signContainer}>
-                        <a href='/'>Register</a>
-                        <a href='/' className='aButtonRegister'>SignUp</a>
+                        {
+                            navyLinksArray.map((item, index) => {
+                                return <a href='/' key={index}>{item}</a>
+                            })
+                        }
                     </div>
 
+                    <div className={styles.signContainer}>
+                        {
+                            navyButtonsArray.map((item, index) => {
+                                if (!item.class) {
+                                    return <a href='/' key={index} >{item.title}</a>
+                                } else {
+                                    return <a href='/' key={index} className={item.class[0]}>{item.title}</a>
+                                }
+                            })
+                        }
+                    </div>
                 </div>
 
                 <div className={styles.burgerMenu}>
                     <div className={styles.burgerMenuIco}></div>
                     <div className={styles.menuExpandedContainer}>
                         <div className={styles.menuExpandedContent}>
-                            <a href='/'>Features</a>
-                            <a href='/'>Pricing</a>
-                            <a href='/'>Resources</a>
+                            {
+                                navyLinksArray.map((item, index) => {
+                                    return <a href='/' key={index}>{item}</a>
+                                })
+                            }
                             <div className={styles.menuHr}></div>
-                            <a href='/'>Register</a>
-                            <a href='/' className={styles.aButtonRegisterDropMenu}>SignUp</a>
+                            {
+                                navyButtonsArray.map((item, index) => {
+                                    if (!item.class) {
+                                        return <a href='/' key={index} >{item.title}</a>
+                                    } else {
+                                        return <a href='/' key={index} className={styles[item.class[1]]}>{item.title}</a>
+                                    }
+                                })
+                            }
                         </div>
                     </div>
                 </div>
 
             </div>
-
-
         </Fragment>
     );
 };
