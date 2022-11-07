542a3b5dd74aea2d9886ddd95518b1f4fa3c8ddb
83mbyte <registered.mail.mb@gmail.com>
2022-11-07 15:57:51 +0200

    localStorage support added

--- src/components/Sections/Shorter/Shorter.js
+++ src/components/Sections/Shorter/Shorter.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useRef, useState } from 'react';
+import React, { Fragment, useEffect, useRef, useState } from 'react';
 import styles from './Shorter.module.css';
 import ShorterResultItem from './ShorterResultItem';
 
@@ -8,6 +8,29 @@ const Shorter = () => {
     const inputContainerRef = useRef();
     // let x = { original: 'http://test_only.com', short: "http://test_only.shrt" };
     const [itemsResult, setItemsResult] = useState([]);
+
+    useEffect(() => {
+
+        if (storageCheck('localStorage')) {
+            if (localStorage.getItem('shorts')) {
+
+                try {
+                    let now = Date.now();
+                    let oldValues = JSON.parse(localStorage.getItem('shorts'));
+                    let filteredValues = oldValues.filter(item => item.expire > now);
+                    localStorage.removeItem('shorts');
+
+                    if (filteredValues.length > 0) {
+
+                        setItemsResult(filteredValues);
+                        localStorage.setItem('shorts', JSON.stringify(filteredValues));
+                    }
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+        }
+    }, []);
 
     const fetchData = async (url) => {
         console.log('...fetching...')
@@ -25,9 +48,34 @@ const Shorter = () => {
             return 'Error'
         }
     }
+    const storageCheck = (type) => {
+        let storage;
+        try {
+            storage = window[type];
+            const x = '__storage_test__';
+            storage.setItem(x, x);
+            storage.removeItem(x);
+            return true;
+        } catch (e) {
+            return e instanceof DOMException && (
+                // everything except Firefox
+                e.code === 22 ||
+                // Firefox
+                e.code === 1014 ||
+                // test name field too, because code might not be present
+                // everything except Firefox
+                e.name === 'QuotaExceededError' ||
+                // Firefox
+                e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+                // acknowledge QuotaExceededError only if there's something already stored
+                (storage && storage.length !== 0);
+        }
+    }
 
     const clickHandler = async (e) => {
         e.preventDefault();
+        let scheduleToExpire = (Date.now() + (60 * 60 * 1000));  //time to expire storage item.. where 60*60*1000 = 1hour.
+
         const urlRegex = /^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/;
 
         if (!inputRef.current.value || inputRef.current.value === '' || !urlRegex.test(inputRef.current.value)) {
@@ -47,11 +95,30 @@ const Shorter = () => {
                         ...itemsResult,
                         { original: result.original, short: result.short }
                     ]
-                )
+                );
+                if (storageCheck('localStorage')) {
+                    if (localStorage.getItem('shorts') && localStorage.getItem('shorts').length > 0) {
+
+                        let oldValues = JSON.parse(localStorage.getItem('shorts'));
+
+                        localStorage.setItem('shorts',
+                            JSON.stringify(
+                                [...oldValues,
+                                { original: result.original, short: result.short, expire: scheduleToExpire }
+                                ]
+                            ))
+                    } else {
+                        localStorage.setItem('shorts', JSON.stringify([{ original: result.original, short: result.short, expire: scheduleToExpire }]))
+                    }
+                }
             }
         }
 
     }
+
+
+
+
     return (
         <Fragment>
             <div className={styles.container}>
